99948d570a8f5b7497524bd1cc74b22c418e4b1d
Forhad <forhaduddin823@gmail.com>
2026-06-12 23:34:08 +0600

    Database Schema Design completed for 2 table

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,11 @@ import express, {
 
 // import { userRoute } from "./modules/user/user.route";
 // import { profileRoute } from "./modules/profile/profile.route";
-// import { authRoute } from "./modules/auth/auth.route";
 import logger from "./middleware/logger";
 // import CookieParser from "cookie-parser";
 import cors from "cors";
 import globalErrorHandler from "./middleware/globalErrorHandler";
+import { authRoute } from "./modules/auth/auth.route";
 
 const app: Application = express();
 
@@ -42,8 +42,8 @@ app.get("/", (req: Request, res: Response) => {
 /* 
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
-app.use("/api/auth", authRoute); 
 */
+app.use("/api/auth", authRoute); 
 
 // Global Error Handling Middleware
 app.use(globalErrorHandler);
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,4 @@
+export const authService = {
+//   loginUserIntoDB,
+//   generateRefreshToken,
+};
